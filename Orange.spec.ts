d38a81181d0f5d04c819b6d331abd602a6fba40d
Durga <dd1142002@gmail.com>
2025-06-12 08:09:40 +0530

    Update Orange.spec.ts

--- Orange.spec.ts
+++ Orange.spec.ts
@@ -1,12 +1,14 @@
 import { test } from "./Fixtures/FixturesPage";
 
 test.describe("orangeHRM", () => {
-  test("login", async ({ loginPage ,jopPage}) => {
+  test("login", async ({ loginPage}) => {
     await loginPage.login();
-    await jopPage.createJob();
+    
   });
   
-  // test("createjob",async({jopPage})=>{
-     // });
+   test("createjob",async({loginPage, jopPage})=>{
+      await loginPage.login();
+    await jopPage.createJob();
+     });
 
 });
